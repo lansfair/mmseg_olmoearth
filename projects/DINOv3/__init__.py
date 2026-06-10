49ae4ee663b4360d26cf568590af6fc8dbccb975
JayWang <62465258+wangjieh@users.noreply.github.com>
2026-06-10 18:20:55 +0800

    Update __init__.py

--- projects/DINOv3/__init__.py
+++ projects/DINOv3/__init__.py
@@ -1,1 +1,5 @@
-from .models import *  # noqa: F401,F403
+"""DINOv3 project extensions for MMSegmentation."""
+
+from . import datasets, hooks, models  # noqa: F401
+
+__all__ = ['datasets', 'hooks', 'models']
