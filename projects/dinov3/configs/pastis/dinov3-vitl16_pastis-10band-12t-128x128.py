b51a54adce5d178a49c871fdb42f7ad0abd0a30e
ZJ <lansfair@users.noreply.github.com>
2026-06-11 14:02:31 +0800

    Increase DINOv3 PASTIS batch size

--- projects/dinov3/configs/pastis/dinov3-vitl16_pastis-10band-12t-128x128.py
+++ projects/dinov3/configs/pastis/dinov3-vitl16_pastis-10band-12t-128x128.py
@@ -48,7 +48,7 @@ test_pipeline = [
 ]
 
 train_dataloader = dict(
-    batch_size=2,
+    batch_size=16,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type="DefaultSampler", shuffle=True),
@@ -64,7 +64,7 @@ train_dataloader = dict(
 )
 
 val_dataloader = dict(
-    batch_size=2,
+    batch_size=16,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type="DefaultSampler", shuffle=False),
@@ -80,7 +80,7 @@ val_dataloader = dict(
 )
 
 test_dataloader = dict(
-    batch_size=2,
+    batch_size=16,
     num_workers=4,
     persistent_workers=True,
     sampler=dict(type="DefaultSampler", shuffle=False),
@@ -199,4 +199,4 @@ default_scope = "mmseg"
 log_level = "INFO"
 load_from = None
 resume = False
-auto_scale_lr = dict(enable=False, base_batch_size=2)
+auto_scale_lr = dict(enable=False, base_batch_size=16)
